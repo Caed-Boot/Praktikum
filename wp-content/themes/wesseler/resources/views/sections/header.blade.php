df1a7767863fe23896b1a04e0bf9cda9c11e7e18
Gabriel Da Silva <gabrieldasilva8542@gmail.com>
2024-09-16 10:59:24 +0200

    creating the navigation

--- wp-content/themes/wesseler/resources/views/sections/header.blade.php
+++ wp-content/themes/wesseler/resources/views/sections/header.blade.php
@@ -6,7 +6,7 @@
 @endphp
 
 
-<header class="flex fixed bg-white w-full justify-between md:justify-between z-40 py-4 shadow-xl header">
+<header class="flex fixed bg-white w-full justify-between z-40  shadow-xl header">
     
 
     <a class="flex justify-center items-center brand ml-6 md:w-40 lg:w-40" href="{{ home_url('/') }}">
@@ -27,7 +27,7 @@
           viewBox="0 0 490 490" xml:space="preserve">
       </div>
 
-      <nav class=" flex-col bg-gray  items-center hidden-menu w-full h-full transition-all duration-700 fixed z-40 p-10 md:static md:p-2 md:bg-white border overflow-y-scroll primary-nav">
+      <nav class=" flex-col bg-gray  items-center hidden-menu w-full h-full transition-all duration-700 fixed z-40 p-10 overflow-y-scroll  md:static md:p-8 md:bg-white  md:overflow-y-visible  primary-nav">
 
           {{-- CLOSE MENU ✖️ --}}
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-turquoise w-10 h-10 ml-auto cursor-pointer close-menu md:hidden">
@@ -42,14 +42,14 @@
             
           {!! wp_nav_menu([
             'theme_location' => 'primary_navigation',
-            'menu_class' => 'flex flex-col gap-8 text-slate-100 capitalize text-xl md:text-2xl mt-20 second-hidden-submenu',
+            'menu_class' => 'flex flex-col gap-8 md:gap-5 text-slate-100 capitalize text-xl md:text-2xl mt-20',
             'after' => '
             
-            <span class="inline-block"> 
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-turquoise">
+            <div class="inline-block"> 
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-turquoise cursor-pointer relative top-1 transition-all duration-150">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
               </svg>
-            </span>
+            </div>
              
 
 ',
